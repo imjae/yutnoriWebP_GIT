122833f2be51b043947e03c3be3859f3d91438fc
JK <jakeim1111@gmail.com>
2018-11-04 18:38:07 +0900

    charInfo --> empty no

--- src/main/java/user/dao/UserDAO.java
+++ src/main/java/user/dao/UserDAO.java
@@ -13,52 +13,77 @@ import user.bean.UserDTO;
 
 @Repository
 public class UserDAO {
-	
+
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-	
-	
+
 	public UserDTO userLogin(UserDTO dto) {
 		return sqlSession.selectOne("mybatis.userMapper.userLogin", dto);
 	}
-	
+
 	public int userSignUp(UserDTO dto) {
 		return sqlSession.insert("mybatis.userMapper.userSignUp", dto);
 	}
-	
+
 	public int userIdCheck(String user_id) {
 		return sqlSession.selectOne("mybatis.userMapper.userIdCheck", user_id);
 	}
+
 	public int userPwCheck(String user_password) {
 		return sqlSession.selectOne("mybatis.userMapper.userPwCheck", user_password);
 	}
+
 	public int userNameCheck(String user_name) {
 		return sqlSession.selectOne("mybatis.userMapper.userNameCheck", user_name);
 	}
+
 	public int userJuminCheck(String user_jumin) {
 		return sqlSession.selectOne("mybatis.userMapper.userJuminCheck", user_jumin);
 	}
+
 	public int userPhoneCheck(String user_phone) {
 		return sqlSession.selectOne("mybatis.userMapper.userPhoneCheck", user_phone);
 	}
+
 	public UserDTO findIdPwSelect(UserDTO dto) {
 		return sqlSession.selectOne("mybatis.userMapper.findIdPwSelect", dto);
 	}
+
 	public UserDTO userIdPwCheck(UserDTO dto) {
 		return sqlSession.selectOne("mybatis.userMapper.userIdPwCheck", dto);
 	}
-	
+
 	public int userModify(UserDTO dto) {
-		
-		Map<String,String> map = new HashMap<String,String>();
+
+		Map<String, String> map = new HashMap<String, String>();
 		map.put("ses_id", dto.getUser_id());
 		map.put("re_pw", dto.getUser_password());
 		return sqlSession.update("mybatis.userMapper.userModify", map);
 	}
-	
-	public List<PaymentHistoryDTO> haveItemList(String user_id) {
-		
-		return sqlSession.selectList("mybatis.userMapper.haveItemList", user_id);
+
+	public List<PaymentHistoryDTO> haveItemList(String category, String user_id, int startNum, int endNum) {
+
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		map.put("category", category);
+		map.put("user_id", user_id);
+		map.put("startNum", startNum);
+		map.put("endNum", endNum);
+
+		return sqlSession.selectList("mybatis.userMapper.haveItemList", map);
 	}
-	
+
+	public List<PaymentHistoryDTO> haveItemListAll(String user_id, int startNum, int endNum) {
+
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		map.put("user_id", user_id);
+		map.put("startNum", startNum);
+		map.put("endNum", endNum);
+
+		return sqlSession.selectList("mybatis.userMapper.haveItemListAll", map);
+	}
+
+	public int haveItemCount(String user_id) {
+		return sqlSession.selectOne("mybatis.userMapper.havaItemCount", user_id);
+	}
+
 }
